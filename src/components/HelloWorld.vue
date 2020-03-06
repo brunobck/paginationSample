b0e17553d25a5544dde844c8586f79b6e1ba6a29
Bruno Galvão <bgalvao.work2@gmail.com>
2020-03-06 15:33:17 -0300

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,146 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <v-flex>
+      <v-data-iterator
+        content-tag="v-layout"
+        content-props="row wrap"
+        :items="productsList"
+        :page.sync="page"
+        :items-per-page="itemsPerPage"
+        hide-default-footer
+        :footer-props="{
+        itemsPerPageOptions: rowsPerPageItems
+      }"
+        @page-count="pageCount = $event"
+      >
+        <v-flex slot="item" slot-scope="props">
+          <v-card>
+            <v-card-title>
+              <h4>{{ props.item.first }}</h4>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-list dense>
+              <v-list-tile>
+                <v-list-tile-content>Detalhe Produto:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.first }}</v-list-tile-content>
+              </v-list-tile>
+              <br />
+              <v-list-tile>
+                <v-list-tile-content>Valor Produto:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.second }}</v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-card>
+        </v-flex>
+      </v-data-iterator>
+      <v-pagination v-model="page" :length="pageCount"></v-pagination>
+    </v-flex>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader"
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify"
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify"
+      }
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com"
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com"
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify"
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs"
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify"
+      }
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ],
+    page: 1,
+    pageCount: 0,
+    itemsPerPage: 2,
+    rowsPerPageItems: [1, 2, 3],
+    pagination: {
+      rowsPerPage: 1,
+      totalItems: 6
+    },
+    headers: [
+      {
+        text: "First collumn",
+        align: "start",
+        sortable: false,
+        value: "first"
+      },
+      {
+        text: "Second collumn",
+        align: "start",
+        sortable: false,
+        value: "second"
+      }
+    ],
+    productsList: [
+      {
+        first: "Produto 1",
+        second: 1
+      },
+      {
+        first: "Produto 2",
+        second: 2
+      },
+      {
+        first: "Produto 3",
+        second: 3
+      },
+      {
+        first: "Produto 4",
+        second: 4
+      },
+      {
+        first: "Produto 5",
+        second: 5
+      },
+      {
+        first: "Produto 6",
+        second: 6
+      }
+    ]
+  })
+};
+</script>
